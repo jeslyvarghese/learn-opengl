c26140952af39f7aae4be27c6d07e54ff2f41362
jesly <jesly.varghese@gmail.com>
2018-04-14 20:53:48 +0530

    Initial Commit

---
+++ OpenGL Lessons/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OpenGL Lessons
+//
+//  Created by Jesly Varghese on 14/04/18.
+//  Copyright © 2018 Jesly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
